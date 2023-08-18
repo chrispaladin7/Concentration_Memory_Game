81be767f081b462acdca8faed32e8c92441e78ec
chrispaladin7 <chrismartey@yahoo.com>
2023-08-18 19:38:43 -0400

    Commit init() and render() and image in html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 	/*----- constants -----*/
-
+    const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
+    const CARD_LOOKUP={
+        card1:"1",
+        card2:"2",
+        card3:"3",
+        card4:"4",
+        card4:"5",
+        card5:"6",
+        card7:"7",
+        card8:"8",
+        
+    }
 
 	/*----- state variables -----*/
 
@@ -11,3 +22,14 @@
 
 
 	/*----- functions -----*/
+
+    init();
+
+
+    function init(){
+
+        render();
+
+    }
+
+    function render();
